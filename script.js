f9a2c398b9fbf65efc2ce1daebc945a0b3ce6ac6
Ccafecitos <michi.juanharry@gmail.com>
2025-07-14 14:06:02 -0400

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+// Aquí definimos qué ramos dependen de cuáles
 const prerrequisitos = {
   8: [1],
   9: [3],
   10: [4],
   12: [8],
+  16: [3],
+  17: [15],
   18: [13],
   19: [14],
-  // Agrega más relaciones según tu malla
+  23: [13],
+  24: [19],
+  25: [20],
+  29: [18, 23],
+  30: [26],
+  31: [20],
+  35: [18, 23],
+  36: [29, 32],
+  37: [25, 31],
+  42: [35, 39],
+  43: [37],
+  44: [38],
+  47: Array.from({ length: 46 }, (_, i) => i + 1),
+  50: [47]
 };
 
 const aprobados = new Set();
 
+// Esta función actualiza los estilos de todos los ramos
 function actualizarEstado() {
   document.querySelectorAll('.ramo').forEach(div => {
     const id = parseInt(div.id);
@@ -29,11 +46,13 @@ function actualizarEstado() {
   });
 }
 
+// Lógica al hacer clic en un ramo
 function aprobar(id) {
   aprobados.add(id);
   actualizarEstado();
 }
 
+// Al cargar la página, inicializamos los estados
 window.onload = () => {
   actualizarEstado();
 };
